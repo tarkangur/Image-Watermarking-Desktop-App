ebcd51bb1f4379d563092bdff6a85cf07d6a0f8b
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-09-18 23:44:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,34 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-
-def UploadAction():
-    file_types = [("Jpg Files", "*.jpg"), ("PNG Files", "*.png")]
-    filename = filedialog.askopenfilename(filetypes=file_types)
-    if filename:
-        print("Selected file:", filename)
-        img = Image.open(filename)
-        img_width, img_height = img.size
-        canvas.config(width=img_width, height=img_height)
-        img = ImageTk.PhotoImage(img)
-        canvas.create_image(img_width // 2, img_height // 2, image=img)
-        canvas.image = img
-
-
 window = tk.Tk()
 window.config(padx=50, pady=50, bg="white")
 window.title("Image Watermarking App")
+img = None
+
+
+def UploadAction():
+    global img
+    file_types = [("Jpg Files", "*.jpg"), ("PNG Files", "*.png")]
+    filename = filedialog.askopenfilename(filetypes=file_types)
+    if filename:
+        img = Image.open(filename)
+        img = ImageTk.PhotoImage(img)
+
+        window.bind("<Configure>")
+        label_photo.config(image=img)
+        label_photo.image = img
+
 
 upload_button = tk.Button(window, text="Upload Image", command=UploadAction)
-upload_button.grid(column=1, row=1, pady=100)
+upload_button.pack(side=tk.BOTTOM)
 
-canvas = tk.Canvas(window, bg="white", highlightthickness=0)
-canvas.grid(column=1, row=0)
+label_photo = tk.Label(window)
+label_photo.pack()
 
 
 window.mainloop()
+
+
+
+
